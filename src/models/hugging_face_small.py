6a9ae3ebfb0deef8f21ea9d97b6a5cea2f91f628
Anupam Kumar <kyteinsky@gmail.com>
2023-10-20 07:45:50 +0530

    use updated yaml parsing fn
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- src/models/hugging_face_small.py
+++ src/models/hugging_face_small.py
@@ -1,10 +1,11 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 # from langchain.llms import HuggingFacePipeline
-import ruamel.yaml as yaml
+from ruamel.yaml import YAML
 
 # this will raise an exception and is intended
 with open('config.yaml') as f:
-	config = yaml.safe_load(f)
+	yaml = YAML(typ='safe')
+	config = yaml.load(f)
 
 _embedder = lambda: HuggingFaceEmbeddings(**config.get('hugging_face_small_embedder', {}))
 
--- src/models/llama.py
+++ src/models/llama.py
@@ -1,10 +1,11 @@
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.llms import LlamaCpp
-import ruamel.yaml as yaml
+from ruamel.yaml import YAML
 
 # this will raise an exception and is intended
 with open('config.yaml') as f:
-	config = yaml.safe_load(f)
+	yaml = YAML(typ='safe')
+	config = yaml.load(f)
 
 _embedder = lambda: LlamaCppEmbeddings(**config.get('llama_embedder', {}))
 _llm = lambda: LlamaCpp(**config.get('llama_llm', {}))
