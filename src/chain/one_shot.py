f053c6934938cef491d9455e22925bc25ce629b3
Anupam Kumar <kyteinsky@gmail.com>
2023-10-30 17:34:31 +0530

    optional usage of user data in llm query
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- src/chain/one_shot.py
+++ src/chain/one_shot.py
@@ -2,19 +2,25 @@ from typing import Any
 from langchain.vectorstores import VectorStore
 from langchain.llms.base import LLM
 
-_LLM_TEMPLATE = """Answer based on this context and do not add any imaginative details:
+_LLM_TEMPLATE = """Answer based only on this context and do not add any imaginative details:
 {context}
 
 {question}
 """
 
 
-def process_query(vectordb: VectorStore, llm: LLM, query: str, limit: int = 5) -> Any | None:
+def process_query(
+		vectordb: VectorStore,
+		llm: LLM,
+		query: str,
+		use_context: bool = True,
+		limit: int = 5
+	) -> Any | None:
+	if not use_context:
+		return llm.predict(query)
+
 	context_docs = vectordb.similarity_search(query, k=limit)
-	print(f"Context docs: {context_docs}")
-
 	context_text = "\n".join(map(lambda d: d.page_content, context_docs))
-	output = llm.predict(_LLM_TEMPLATE.format(context=context_text, question=query))
-	print(f'Output: {output}')
 
+	output = llm.predict(_LLM_TEMPLATE.format(context=context_text, question=query))
 	return output
