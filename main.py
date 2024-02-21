d83435368161a651ccb51dd4b43e3797636574e4
Anupam Kumar <kyteinsky@gmail.com>
2024-02-21 20:05:44 +0530

    no colors for CI exec
    USE_COLORS can be used to turn off colors in non-CI envs
    
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+from os import getenv
 
 import uvicorn
 
@@ -8,15 +8,16 @@ from context_chat_backend.utils import to_int
 if __name__ == '__main__':
 	uvicorn.run(
 		app='context_chat_backend:app',
-		host=os.getenv('APP_HOST', '0.0.0.0'),
-		port=to_int(os.getenv('APP_PORT'), 9000),
+		host=getenv('APP_HOST', '0.0.0.0'),
+		port=to_int(getenv('APP_PORT'), 9000),
 		http='h11',
 		interface='asgi3',
-		log_level=('warning', 'trace')[os.getenv('DEBUG', '0') == '1'],
-		use_colors=True,
+		log_level=('warning', 'trace')[getenv('DEBUG', '0') == '1'],
+		use_colors=bool(getenv('USE_COLORS', '1') == '1' and getenv('CI', 'false') == 'false'),
 		# limit_concurrency=10,
 		# backlog=20,
 		timeout_keep_alive=120,
 		h11_max_incomplete_event_size=5 * 1024 * 1024,  # 5MiB
-		workers=to_int(os.getenv('UVICORN_WORKERS'), 1),
+		# todo: on-demand instantiation of the resources for multi-worker mode
+		workers=to_int(getenv('UVICORN_WORKERS'), 1),
 	)
