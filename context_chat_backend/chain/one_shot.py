adfea338f442ef8be961f089bd533aaa7c670775
Anupam Kumar <kyteinsky@gmail.com>
2024-04-13 04:26:23 +0530

    feat: no context generation is now a chat completion
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

--- context_chat_backend/chain/one_shot.py
+++ context_chat_backend/chain/one_shot.py
@@ -28,14 +28,17 @@ def process_query(
 	scope_type: ScopeType | None = None,
 	scope_list: list[str] | None = None,
 	template: str | None = None,
+	no_ctx_template: str | None = None,
 	end_separator: str = '',
 ) -> tuple[str, list[str]]:
 	if not use_context:
-		return llm.predict(query), []
+		stop = [end_separator] if end_separator else None
+		return llm.invoke((query, no_ctx_template.format(question=query))[no_ctx_template is not None], stop=stop), []
 
 	user_client = vectordb.get_user_client(user_id)
 	if user_client is None:
-		return llm.predict(query), []
+		stop = [end_separator] if end_separator else None
+		return llm.invoke((query, no_ctx_template.format(question=query))[no_ctx_template is not None], stop=stop), []
 
 	context_docs = None
 	if not_none(scope_type) and not_none(scope_list) and len(scope_list) > 0:
@@ -55,8 +58,10 @@ scope list: {scope_list}\n\nproceeding with an unscoped query')
 
 	context_text = '\n\n'.join(f'{d.metadata.get("title")}\n{d.page_content}' for d in context_docs)
 
-	output = llm.predict((template or _LLM_TEMPLATE).format(context=context_text, question=query)) \
-		.strip().rstrip(end_separator).strip()
+	output = llm.invoke(
+		(template or _LLM_TEMPLATE).format(context=context_text, question=query),
+		stop=[end_separator],
+	).strip()
 	unique_sources: list[str] = list({source for d in context_docs if (source := d.metadata.get('source'))})
 
 	return (output, unique_sources)
