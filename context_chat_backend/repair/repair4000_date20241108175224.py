274ad0edf19860a6030e5507c1870a0e51e786d6
Anupam Kumar <kyteinsky@gmail.com>
2024-11-08 18:01:25 +0530

    fix: reset the vector db in favour of a new embedding model
    Signed-off-by: Anupam Kumar <kyteinsky@gmail.com>

---
+++ context_chat_backend/repair/repair4000_date20241108175224.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+'''
+Reset the vector db in favour of a new embedding model
+'''
+
+vector_db_path = os.path.join(
+	os.getenv('APP_PERSISTENT_STORAGE', 'persistent_storage'),
+	'vector_db_data',
+)
+
+if os.path.exists(vector_db_path):
+	shutil.rmtree(vector_db_path)
+	os.makedirs(vector_db_path, mode=0o770)
